8cb718498e742ae36f2e57a9f37764164f878b62
Berkeley Martinez <Berkeley@robotie.com>
2016-04-01 23:03:22 -0700

    Use source maps in dev only

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,20 +1,6 @@
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-
 exports.modifyWebpackConfig = function modifyWebpackConfig(config, env) {
-  config.merge({ devtool: 'inline-source-map' });
-  if (env === 'static') {
-    config.removeLoader('css');
-    config.loader('css', function(cfg) {
-      cfg.test = /\.css$/;
-      cfg.loader = ExtractTextPlugin.extract('css?minimize');
-      return cfg
-    });
-    config.plugin(
-      'extract-css',
-      ExtractTextPlugin,
-      [ 'styles.css' , { allChunks: true } ]
-    );
+  if (process.env.NODE_ENV !== 'production') {
+    config.merge({ devtool: 'inline-source-map' });
   }
   return config;
 }
