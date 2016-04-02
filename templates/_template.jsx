5ae0648bd8279c90a9846d1a3b1b49e6c3392a04
Berkeley Martinez <Berkeley@robotie.com>
2016-04-02 01:20:35 -0700

    Add url message up to parent when in iframe

--- templates/_template.jsx
+++ templates/_template.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import { Link, State, Navigation } from 'react-router';
+import { browserHistory } from 'react-router'
 import { Breakpoint } from 'react-responsive-grid';
-import includes from 'underscore.string/include';
 
 // Style code
 import 'css/github.css';
 import 'css/gatsby.css';
 
 module.exports = React.createClass({
+  componentDidMount() {
+    // if iframe, this will be the parent url
+    const originUrl = typeof document !== 'undefined' ?
+      document.referrer :
+      false;
+    // grab parent DOM object to post messages
+    const parent = typeof window !== 'undefined' ?
+      window.parent :
+      false;
+    if (!parent && !originUrl) {
+      return;
+    }
+    // listen to react-router history changes
+    // if change happens, post message to parent window
+    // up to parent to update url
+    browserHistory.listen(location => {
+      parent.postMessage(location.pathname, originUrl);
+    });
+  },
   render: function() {
     return (
       <div className='wiki-container'>
--- templates/lang/_template.jsx
+++ templates/lang/_template.jsx
@@ -101,5 +101,5 @@ module.exports = React.createClass({
         </div>
       </div>
     )
-  },
+  }
 });
