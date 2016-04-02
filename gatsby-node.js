83082fc28c89eb8ed3d3d21be7ded3e850dece60
Berkeley Martinez <Berkeley@robotie.com>
2016-04-02 01:18:16 -0700

    Don't add source-maps in production

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,8 @@
 exports.modifyWebpackConfig = function modifyWebpackConfig(config, env) {
   if (process.env.NODE_ENV !== 'production') {
     config.merge({ devtool: 'inline-source-map' });
+  } else {
+    config.merge({ devtool: false });
   }
   return config;
 }
