eb824bbd11e5beac492e161fc015a0ef08a04902
Rafael J. Rodriguez <rafase282@gmail.com>
2016-05-19 09:57:58 -0400

    Make img responsive and searchbar max width

--- templates/lang/_template.jsx
+++ templates/lang/_template.jsx
@@ -1,99 +1,72 @@
 import React from 'react';
-import { Link, State, Navigation } from 'react-router';
+import {Link, State, Navigation} from 'react-router';
 import find from 'lodash/find';
-import { prefixLink } from 'gatsby-helpers';
+import {prefixLink} from 'gatsby-helpers';
 import pageList from './_pages.yaml';
 
-const langRegex = new RegExp( '^/' + __filename.slice(0,2));
+const langRegex = new RegExp('^/' + __filename.slice(0, 2));
 module.exports = React.createClass({
   displayName: 'Page',
-  propTypes () {
-    return {
-      route: React.PropTypes.object,
-    }
+  propTypes() {
+    return {route: React.PropTypes.object}
   },
   contextTypes: {
-    router: React.PropTypes.object.isRequired,
+    router: React.PropTypes.object.isRequired
   },
-  handleTopicChange (e) {
+  handleTopicChange(e) {
     return this.context.router.push(e.target.value)
   },
-   getInitialState () {
-    return {
-      filterText: ''
-    };
+  getInitialState() {
+    return {filterText: ''};
   },
-  handleSearchChange (e) {
-    this.setState({
-      filterText: this.refs.filterTextInput.value
-    });
+  handleSearchChange(e) {
+    this.setState({filterText: this.refs.filterTextInput.value});
   },
   renderList(pages) {
-    return pages
-      .filter(child => {
-        if (this.state.filterText.length > 0) {
-          let regex = new RegExp(this.state.filterText, 'i');
-          return regex.test(child.title);
-        } else {
-          return true;
-        }
-      })
-      .map(child => {
-        const isActive = prefixLink(child.path) === this.props.location.pathname
-        return (
-          <li key={child.path}>
-            <Link to={ prefixLink(child.path)}>
-              {isActive ? <strong>{child.title}</strong> : child.title }
-            </Link>
-          </li>
-        );
-      });
+    return pages.filter(child => {
+      if (this.state.filterText.length > 0) {
+        let regex = new RegExp(this.state.filterText, 'i');
+        return regex.test(child.title);
+      } else {
+        return true;
+      }
+    }).map(child => {
+      const isActive = prefixLink(child.path) === this.props.location.pathname
+      return (
+        <li key={child.path}>
+          <Link to={prefixLink(child.path)}>
+            {isActive
+              ? <strong>{child.title}</strong>
+              : child.title}
+          </Link>
+        </li>
+      );
+    });
   },
   renderSelector(pages) {
-    return pages
-      .map(child =>
-        <option
-          key={ prefixLink(child.path) }
-          value={ prefixLink(child.path) }
-        >
-          { child.title }
-        </option>
-      );
+    return pages.map(child => <option key={prefixLink(child.path)} value={prefixLink(child.path)}>
+      {child.title}
+    </option>);
   },
-  render () {
+  render() {
     const childPages = pageList.map((p) => {
       return find(this.props.route.pages, (_p) => _p.path === p)
-    })
-      .filter(Boolean)
-      .map(page => ({
-        title: page.data.title,
-        order: page.data.order,
-        path: page.path
-      }))
-      .sort((a,b) => a.title.localeCompare(b.title));
+    }).filter(Boolean).map(page => ({title: page.data.title, order: page.data.order, path: page.path})).sort((a, b) => a.title.localeCompare(b.title));
     return (
       <div>
         <div className='wiki-aside'>
           <div className='searchBar'>
-            <input
-              type='text'
-              value ={this.props.filterText}
-              ref='filterTextInput'
-              onChange={this.handleSearchChange}
-              placeholder='Article search'
-            />
+            <input className='searchBar' type='text' value ={this.props.filterText} ref='filterTextInput' onChange={this.handleSearchChange} placeholder='Article search'/>
           </div>
           <div className='articlesList'>
             <ul>
-              { this.renderList(childPages) }
+              {this.renderList(childPages)}
             </ul>
           </div>
         </div>
         <div className='wiki-selector'>
-          <select
-            defaultValue={this.props.location.pathname}
-            onChange={this.handleTopicChange}>
-            { this.renderSelector(childPages) }
+          <select defaultValue={this.props.location.pathname} onChange={this.handleTopicChange}>
+            {this.renderSelector(childPages)}
           </select>
         </div>
         <div className='articleContent'>
